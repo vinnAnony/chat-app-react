db9d87bebebd1c08cac211efcab4cd300e923b8d
Vincent Mbugua <vnjeru85@gmail.com>
2022-06-29 15:30:10 +0300

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBxxCUOD0Qa9df_FsbqT3ffnHjr1w9LHNw",
+    authDomain: "whatsapp-mern-656c3.firebaseapp.com",
+    projectId: "whatsapp-mern-656c3",
+    storageBucket: "whatsapp-mern-656c3.appspot.com",
+    messagingSenderId: "727501243192",
+    appId: "1:727501243192:web:94f8c1df00a37bf41937cd"
+  };
+  
