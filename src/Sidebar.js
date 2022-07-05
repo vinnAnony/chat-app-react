4b2c9710c8fcf60cf8c64365b81c4901dee86607
VEGA/VNM <vnjeru@vegasoftware.com>
2022-07-05 18:00:20 +0300

    begin sidebar chat component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -22,11 +22,16 @@ function Sidebar() {
           </IconButton>      
         </div>
       </div>
+
       <div className="sidebar__search">
           <div className="sidebar__searchContainer">
             <SearchOutlined/>
             <input placeholder="Search or start new chat" type="text"/>
           </div>
+        </div>
+
+        <div className="sidebar__chats">
+          <SidebarChat/>
         </div>
     </div>
   );
