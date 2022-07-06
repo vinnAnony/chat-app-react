8ad623d404d9d5841d3372712fb11966f1dcb39f
VEGA/VNM <vnjeru@vegasoftware.com>
2022-07-06 13:22:30 +0300

    instantiate functional component

---
+++ src/SidebarChat.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function SidebarChat() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SidebarChat
+
+// https://www.youtube.com/watch?v=ktjafK4SgWM
+// 9:17:35
