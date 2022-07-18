56d78a1d0cff3b18165a06001604421d693090c6
VEGA/VNM <vnjeru@vegasoftware.com>
2022-07-18 15:13:33 +0300

    create node server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+// app config
+const app = express()
+const port = process.env.PORT || 9000
+
+// middleware
+
+// db config
+
+// api routes
+app.get('/',(req,res) => {res.status(200).send('hello world')})
+
+// listen
+app.listen(port,() => {console.log(`Listening to port:${port}`);})
