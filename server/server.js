6b20769d8002d53c97ad388e23e084493660f541
VEGA/VNM <vnjeru@vegasoftware.com>
2022-07-19 10:16:54 +0300

    configure server

--- server/server.js
+++ server/server.js
@@ -5,11 +5,14 @@ const app = express()
 const port = process.env.PORT || 9000
 
 // middleware
+app.use(express.json());
 
 // db config
 
+
 // api routes
-app.get('/',(req,res) => {res.status(200).send('hello world')})
+app.get('/',(req,res) => {res.status(200).send('hello world')});
+
 
 // listen
 app.listen(port,() => {console.log(`Listening to port:${port}`);})
